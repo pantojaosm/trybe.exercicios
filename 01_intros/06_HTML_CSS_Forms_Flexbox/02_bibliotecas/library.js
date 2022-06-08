ee03465010baa7ff8f06a05b8f41ed6edcc4be2c
Pan <pantojaosm@fastmail.com>
2022-06-07 23:07:08 -0400

    Exercício de bibliotecas.

---
+++ 01_intros/06_HTML_CSS_Forms_Flexbox/02_bibliotecas/library.js
@@ -0,0 +1,14 @@
+/* Exemplo de uso da Biblioteca Moment.js 
+
+let moment = require('moment');
+
+let brasilTetra = moment('17071994', 'DDMMYYYY');
+let fromNow = brasilTetra.fromNow();
+
+console.log('Ouvimos o "É TETRAAA!!!" há: ' + fromNow);
+
+*/
+
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+    alert('Voilà!');
+  });
